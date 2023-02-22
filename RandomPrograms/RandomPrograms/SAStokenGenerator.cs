f448564ed60d2ca0448f7b152670e4d91e3e02a7
Ivan F <ivanrvf@gmail.com>
2023-02-22 12:00:23 -0600

    Added comments in code

--- RandomPrograms/RandomPrograms/SAStokenGenerator.cs
+++ RandomPrograms/RandomPrograms/SAStokenGenerator.cs
@@ -21,7 +21,7 @@ namespace RandomPrograms
                 string key = ConfigurationManager.AppSettings["SBprimarykey"].ToString();
                 TimeSpan sinceEpoch = DateTime.UtcNow - new DateTime(1970, 1, 1);
                 var week = 60 * 60 * 24 * 365 * 15;
-                var expiry = Convert.ToString((long)sinceEpoch.TotalSeconds + week);
+                var expiry = Convert.ToString((long)sinceEpoch.TotalSeconds + week); // Changed from int to long to accomodate higher expiry period like 15 years.
                 string stringToSign = HttpUtility.UrlEncode(resourceUri) + "\n" + expiry;
                 HMACSHA256 hmac = new HMACSHA256(Encoding.UTF8.GetBytes(key));
                 var signature = Convert.ToBase64String(hmac.ComputeHash(Encoding.UTF8.GetBytes(stringToSign)));
